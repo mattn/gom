908eff274a5b689a8da727d696c1c681d81b394b
mattn <mattn.jp@gmail.com>
2013-09-11 21:47:24 +0900

    Usage

--- main.go
+++ main.go
@@ -7,16 +7,18 @@ import (
 )
 
 func usage() {
-	fmt.Printf("Usage of %s:\n", os.Args[0])
-	fmt.Println(" Tasks:")
-	fmt.Println("   gom build   [options]")
-	fmt.Println("   gom install [options]")
-	fmt.Println("   gom test    [options]")
-	fmt.Println("   gom run     [options]")
-	fmt.Println("   gom doc     [options]")
-	fmt.Println("   gom exec    [arguments]")
-	fmt.Println("   gom gen travis-yml")
-	fmt.Println("   gom gen gomfile")
+	fmt.Printf(`Usage of %s:
+ Tasks:
+   gom build   [options]   : Build with vendor packages
+   gom install [options]   : Install bundled packages into vendor directory
+   gom test    [options]   : Run tests with bundles
+   gom run     [options]   : Run go file with bundles
+   gom doc     [options]   : Run godoc for bundles
+   gom exec    [arguments] : Execute command with bundle environment
+   gom gen travis-yml      : Generate .travis.yml which uses "gom test"
+   gom gen gomfile         : Scan packages from current directory as root
+                              recursively, and generate Gomfile
+`, os.Args[0])
 	os.Exit(1)
 }
 
