703cbffd1d3896010a79dddbc1f031c30412d387
mattn <mattn.jp@gmail.com>
2013-09-11 11:50:02 +0900

    coloring

--- install.go
+++ install.go
@@ -2,14 +2,48 @@ package main
 
 import (
 	"fmt"
+	"github.com/daviddengcn/go-colortext"
+	"os"
+	"os/exec"
+	"path/filepath"
 )
 
 func install(goms []Gom) error {
+	vendor, err := filepath.Abs("vendor")
+	if err != nil {
+		return err
+	}
+	_, err = os.Stat(vendor)
+	if err != nil {
+		err = os.MkdirAll(vendor, 755)
+		if err != nil {
+			return err
+		}
+	}
+	err = os.Setenv("GOPATH", vendor)
+	if err != nil {
+		return err
+	}
 	for _, gom := range goms {
-		fmt.Printf("installing %s(tag: %s, options: %s)\n",
+		ct.ChangeColor(ct.Cyan, true, ct.None, false)
+		fmt.Printf("installing %s(tag=%s)\n",
 			gom.name,
-			gom.tag,
-			gom.options)
+			gom.tag)
+		ct.ResetColor()
+		args := []string{"go", "get", "-x"}
+		if gom.tag != "" {
+			args = append(args, "-tags", gom.tag)
+		}
+		args = append(args, gom.name)
+		cmd := exec.Command(args[0], args[1:]...)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		ct.ChangeColor(ct.Blue, true, ct.None, false)
+		err = cmd.Run()
+		ct.ResetColor()
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
